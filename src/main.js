1539af1bc13730e33362862770280916b6bf1c90
嵇在春 <1634232332@qq.com>
2022-06-11 16:59:51 +0800

    登录页面完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import './plugins/element.js'
 
 Vue.config.productionTip = false
 
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
