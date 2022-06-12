5182cdc8804203a21530c91168d6fd6cad203388
嵇在春 <1634232332@qq.com>
2022-06-12 08:54:23 +0800

    主分支第一次提交

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
