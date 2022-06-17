b6d3dc0feff9a6c80e9ff21bf158b2f738b5a285
嵇在春 <1634232332@qq.com>
2022-06-17 20:43:37 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,3 +3,8 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element)
+// import Timeline from '@/plugins/timeline/index'
+// import { TimelineItem } from './timeline-item/index'
+//
+// Vue.use(Timeline)
+// Vue.use(TimelineItem)
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
 import List from '@/components/goods/List'
 import Add from '@/components/goods/Add'
+import Order from '@/components/order/Order'
 
 Vue.use(VueRouter)
 
@@ -57,8 +58,12 @@ const router = new VueRouter({
           component: List,
         },
         {
-          path:'/goods/add',
+          path: '/goods/add',
           component: Add,
+        },
+        {
+          path: '/orders',
+          component: Order,
         }
       ]
     },
