13d434a1a3107475f39ef6c21cb9711b596e27c8
嵇在春 <1634232332@qq.com>
2022-06-17 15:58:55 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -5,14 +5,32 @@ import router from './router'
 import './plugins/element.js'
 import TreeTable from 'vue-table-with-tree-grid'
 
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-// Vue.use(ElementUI);
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 Vue.component('tree-table', TreeTable)
 
 Vue.config.productionTip = false
 
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+
+})
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
